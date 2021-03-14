a4522d3379702fcc7066eac042ae8a3206dfe803
Alisson Juan <62479255+AlissonJF@users.noreply.github.com>
2021-03-14 20:47:32 -0300

    QRCode Scanner and Create
    Este é um App criado para estudar a criação e funcionamento do famoso QR Code.

---
+++ lib/page/qr_scan_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:qrcode/widget/buttom_widget.dart';
+
+import 'package:qrcode/main.dart';
+
+class QRScanPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _QRScanPageState();
+}
+
+class _QRScanPageState extends State<QRScanPage> {
+  String qrCode = 'Nenhum encontrado';
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Escaneando Resultado',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                '$qrCode',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 72),
+              ButtonWidget(
+                text: 'Começar a escanear',
+                onClicked: () => scanQRCode(),
+              )
+            ],
+          ),
+        ),
+      );
+
+  Future<void> scanQRCode() async {
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancelar',
+        true,
+        ScanMode.QR,
+      );
+
+      if (!mounted) return;
+
+      setState(() {
+        this.qrCode = qrCode;
+      });
+    } on PlatformException {
+      qrCode = 'Falha ao obter a versão da plataforma.';
+    }
+  }
+}
